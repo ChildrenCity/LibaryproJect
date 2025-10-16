4edbdda3175b3fcda3c8fc6e2caba3bb382b8560
ChildrenCity <8a.13pumipat@gmail.com>
2025-10-16 14:43:15 +0700

    MainWindow

--- app/src/main/java/com/example/projectilm/DataBaseHelper.java
+++ app/src/main/java/com/example/projectilm/DataBaseHelper.java
@@ -14,6 +14,14 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String COL_ID ="ID";
     public static final String COL_Name = "Username";
     public static final String COL_Password = "Password";
+    public static final String BOOK_TABLE = "Books";
+    public static final String BOOK_ID = "BookID";
+    public static final String BOOK_TITLE = "Title";
+    public static final String BOOK_AUTHOR = "Author";
+    public static final String BOOK_COST = "COST";
+
+
+
 
 
     public DataBaseHelper(Context context) {
@@ -28,15 +36,26 @@ public class DataBaseHelper extends SQLiteOpenHelper {
                 COL_Password + " TEXT)";
         db.execSQL(CREATE_USERS);
 
+        String CREATE_BOOK = "CREATE TABLE " + BOOK_TABLE + " (" +
+                BOOK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                BOOK_TITLE + " TEXT, " +
+                BOOK_AUTHOR + " TEXT, " +
+                BOOK_COST + "INTEGER, " +
+                "BookImage TEXT)";
+        db.execSQL("INSERT INTO " + BOOK_TABLE + " (" +
+            BOOK_TITLE + ", " + BOOK_AUTHOR + ", " + BOOK_COST + ", BookImage) VALUES " +
+            "('The Odyssey', 'โฮเมอร์ (Homer)',899, 'https://press.uchicago.edu/.imaging/mte/ucp/medium/dam/ucp/books/jacket/978/02/26/60/9780226604428.jpg/jcr:content/9780226604428.jpg')");
+
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         String DROP_STUDENT_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        String Drop_BOOK_NAME = "DROP TABLE IF EXISTS " + BOOK_TABLE;
         db.execSQL(DROP_STUDENT_TABLE);
+        db.execSQL(Drop_BOOK_NAME);
         onCreate(db);
-
-
     }
 
     public boolean insertUser(String Username, String Password) {
@@ -63,6 +82,10 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public Cursor getAllUsers() {
         SQLiteDatabase db = this.getReadableDatabase();
         return db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+    }
+    public Cursor getAllBooks(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("SELECT * FROM " + BOOK_TABLE,null);
     }
 
 }
